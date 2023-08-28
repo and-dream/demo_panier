5b17355e62b3398c19b5b244e766a5507f409e31
Gloria <gloriadamase@gmail.com>
2023-08-28 16:46:21 +0200

    badge quantity

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -27,6 +27,19 @@ class PanierController extends AbstractController
                 'quantity' => $quantity
             ];
         }
+
+        $total = 0;  //j'initalise mon total
+
+        foreach($cartWithData as $item)
+        {
+            $sousTotal = $item['product']->getPrice() * $item['quantity'];
+            $total += $sousTotal;  // à chaque tour de boucle je calcule le sous-total
+        }
+
+        return $this->render('panier/index.html.twig', [
+            'items' => $cartWithData,
+            'total' => $total
+        ]);
     }
 
     #[Route('/cart/add/{id}', name: "cart_add")]
@@ -40,14 +53,17 @@ class PanierController extends AbstractController
         //paramètre 1: 'cart' récupère la partie réservée au panier
         //paramètre 2: s'il n'y a pas de panier, en créer un vide dans un tableau
         $cart = $session->get('cart', []);
+        $qt = $session->get('qt', 0);
 
         //on vérifie que notre tableau à l'indice du produit qu'il y a qqcho dedans
         //si le produit existe déjà j'incrémente sa quantité sinon j'initialise à 1
         if(!empty($cart[$id]))
         {
             $cart[$id]++;
+            $qt++;
         }else{
-
+            
+            $qt++;
             $cart[$id] = 1;
         }
         //dans la nouvelle (en indice l'Id du produit, en valeur sa quantité)
@@ -56,9 +72,41 @@ class PanierController extends AbstractController
 
         //on sauvegarde l'état du panier en session, on va set notre nouveau tableau avec cet élément
         $session->set('cart', $cart);
+        $session->set('qt', $qt);
         //je sauvegarde l'état de mon panier en session à l'attribut de session "cart"
+
+        return $this->redirectToRoute('accueil');
+
 
         dd($session->get('cart'));
         
+    }
+
+    #[Route('/cart/remove/{id}', name:'cart_remove')]
+    public function remove($id, RequestStack $rs)
+    {
+        $session = $rs->getSession();       //je récupère ma session
+        $cart = $session->get('cart', []) ;                  // dans la session on va chercher celui qui a comme attribut "cart"
+        $qt = $session->get('qt', 0);
+        //s'il n'y a pas de quantité on ne supprime rien dans ma carte à l'indice ID
+        //!si l'id existe dans mon panier, je le supprime du tableau grâce à unset()
+        if(!empty($cart[$id]))
+        {
+            $qt -= $cart[$id];
+            unset($cart[$id]);
+
+        }
+        //gérer l'erreur possible négative
+        if($qt < 0)
+        {
+            $qt = 0;
+        }
+
+        $session->set('qt', $qt);
+
+        //une fois qu'on a supprimé qqch dans le session on doit la mettre à jour
+        $session->set('cart', $cart);                 //on le renvoie dans la nouvelle session | ici cart c'est l'indice dans ma session
+
+        return $this->redirectToRoute('cart');
     }
 }
